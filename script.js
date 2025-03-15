c8abb15d4264586081597f5beb0836df71685109
Autello <31736342+Autello@users.noreply.github.com>
2025-03-15 17:01:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 function calculate() {
   // get input values
-  const product_name = parseFloat(document.getElementById('product_name').value);
   const price = parseFloat(document.getElementById('price').value);
   const weight = parseFloat(document.getElementById('weight').value);
   const total_servings = parseFloat(document.getElementById('total_servings').value);
   const calories_per_serving = parseFloat(document.getElementById('calories_per_serving').value);
+
+  // calculate variables
+
+  let cents = price * 100;
+  let total_calories = calories_per_serving * total_servings;
+  let calorie_price = total_calories / cents;
+  let calorie_weight = total_calories / weight;
+
+  //calculate scores
+
+  let weight_score = (calorie_weight / 240) * 50;
+  let price_score = (calorie_price / 30) * 50;
+  let score = weight_score + price_score;
+
+  //validate inputs
+
+  if (isNaN(price) || isNaN(weight) || isNaN(total_servings) || isNaN(calories_per_serving)) {
+    alert("some fields have been left blank.");
+    return;
+  }
+  
+ if (weight === 0 || price === 0) {
+    alert("Price and weight must be greater than zero.");
+    return;
+  }
+
+  
