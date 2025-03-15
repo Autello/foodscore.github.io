01f1bb0adb51c56e616b46bec221a32923d24f50
Autello <31736342+Autello@users.noreply.github.com>
2025-03-15 16:38:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+function calculate() {
+  // get input values
+  const product_name = parseFloat(document.getElementById('product_name').value);
+  const price = parseFloat(document.getElementById('price').value);
+  const weight = parseFloat(document.getElementById('weight').value);
+  const total_servings = parseFloat(document.getElementById('total_servings').value);
+  const calories_per_serving = parseFloat(document.getElementById('calories_per_serving').value);
